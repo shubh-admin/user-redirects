85241b5aec4a2de9a088b6cb280b981e3d90a31e
devthedeveloper <dhruvgupta1992@hotmail.com>
2015-12-29 12:18:49 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $ = jQuery.noConflict();
 function add_input(a,b)
 {
 	b = b+1;
-	var data = "<p>Previous Link <input type='url' required name='user_redirects["+b+"][pre_link]'> Next Link <input type='url' required name='user_redirects["+b+"][next_link]'> <input type='button'  value='add' onclick='add_input(this,"+b+")'> <input type='button'  value='remove' onclick='remove_input(this)'> </p>";
+	var data = "<p>Older Url <input type='url' required name='user_redirects["+b+"][pre_link]'> New Url <input type='url' required name='user_redirects["+b+"][next_link]'> <input type='button'  value='add' onclick='add_input(this,"+b+")'> <input type='button'  value='remove' onclick='remove_input(this)'> </p>";
 	$(a).parent().after(data);
 }
 
